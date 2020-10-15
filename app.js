2ea9b9ab85c26ba41aa36205841e85628d8a67d3
mairagee524 <garciamaira524@yahoo.com>
2020-10-14 22:47:14 -0700

    added code to JS file to render images on button click

--- app.js
+++ app.js
@@ -27,12 +27,66 @@ const game = () => {
 
         choices.forEach(option => {
             option.addEventListener('click', function() {
-                
+
+                // computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                // here is where we call compareChoices
+                compareChoices(this.textContent, computerChoice);
+
+                // Update Imges
+                playerHand.scr = `./assets/${this.textContent}.png`;
+
+                computerHand.scr = `./assets/${this.textContent}.png`
+
             })
         })
+    }
 
-        const computerNumber = Math.floor(Math.random() * 3);
+    const compareChoices = (playerChoice, computerChoice) => {
 
+        // update text
+        const winner = document.querySelector('.winner');
+
+        // Checking for a tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie!';
+            return; 
+        }
+
+        // Check for a Rock
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins';
+                return;
+            } else {
+                winner.textContent = 'Computer wins';
+                return;
+            }
+        }
+
+        // Check for a Paper
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins';
+                return;
+            } else {
+                winner.textContent = 'Player wins';
+                return;
+            }
+        }
+
+        // Check for a Scissors
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = 'Computer Wins';
+                return;
+            } else {
+                winner.textContent = 'Player wins';
+                return;
+            }
+        }
     }
 
     // call all the inner functions
