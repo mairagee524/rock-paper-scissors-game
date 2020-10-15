01a302bf6858f988930877942728e00fdb96dd32
mairagee524 <garciamaira524@yahoo.com>
2020-10-14 22:15:22 -0700

    added function to buttons

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0; 
 
+
+    // Start the game
     const startGame = () => {
         const playButton = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -9,11 +11,34 @@ const game = () => {
 
         playButton.addEventListener('click', () => {
             introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+
         })
+    }
+
+    // Play Match
+    const playGame = () => {
+        const choices = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        // Computer Options
+        const computerOptions = [ 'rock', 'paper', 'scissors'];
+
+        choices.forEach(option => {
+            option.addEventListener('click', function() {
+                
+            })
+        })
+
+        const computerNumber = Math.floor(Math.random() * 3);
 
     }
 
     // call all the inner functions
+    startGame();
+    playGame();
 }
 
-//
+// start the game function
+game();
